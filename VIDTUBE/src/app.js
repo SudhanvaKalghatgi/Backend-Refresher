ee0f1e762de8f10e8d80875b5ec3343abbee853e
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-01-06 19:25:47 +0530

    HealthCheck ROutes and Testing

--- VIDTUBE/src/app.js
+++ VIDTUBE/src/app.js
@@ -13,7 +13,17 @@ app.use(
 //COMMON MIDDLEWARE
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+//IMPORT ROUTES
+
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+
+
+//ROUTES
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
 export { app };
